bcfe0393c5f98db3e26f7741cba42107d2876689
Polina <polina-bokova@mail.ru>
2024-08-02 15:24:23 +0300

    README.md

---
+++ src/main/java/ru/netology/services/FreelanceService.java
@@ -0,0 +1,20 @@
+package ru.netology.services;
+
+public class FreelanceService {
+
+    public int calculate(int income, int expenses, int threshold) {
+        int money = 0;
+        int count = 0;
+        for (int i = 0; i < 12; i++) {
+            if (money >= threshold) {
+                money = money - expenses;
+                money = money / 3;
+                count ++;
+            } else {
+                money = money + income;
+                money = money - expenses;
+            }
+        }
+        return count;
+    }
+}
